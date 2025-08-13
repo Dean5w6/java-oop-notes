39148fd38e8bfbb6e1ec1fdb124e6d94e66babf0
Dean5w6 <deanjoefrey@gmail.com>
2025-08-13 13:34:36 +0800

    Standard version of `Employee` is-a `Person`

--- src/Employee.java
+++ src/Employee.java
@@ -1,16 +1,14 @@
 public class Employee extends Person {
     private String jobTitle;
 
-    public void setJobTitle(String newJobTitle) {
-        this.jobTitle = newJobTitle;
-    }
-
-    public String getJobTitle() {
-        return this.jobTitle;
+    public Employee(String name, String jobTitle) {
+        super(name);
+        this.jobTitle = jobTitle;
     }
 
     @Override
     public void introduce() {
-        System.out.println("Hello, my name is " + this.name + ", and my job title is " + this.jobTitle);
+        super.introduce();
+        System.out.println(". my job title is " + this.jobTitle);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Employee employee = new Employee();
-
         System.out.printf("Type your name: ");
         String employeeName = scanner.nextLine();
-        employee.name = employeeName;
 
         System.out.printf("Type your job title: ");
         String employeeJobTitle = scanner.nextLine();
-        employee.setJobTitle(employeeJobTitle);
+
+        Employee employee = new Employee(employeeName, employeeJobTitle);
 
         System.out.println("\n");
+
         employee.introduce();
     }
 }
