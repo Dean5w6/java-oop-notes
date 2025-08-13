38cf8797edbd7592add011ca85c52592ae351e51
Dean5w6 <deanjoefrey@gmail.com>
2025-08-13 20:58:17 +0800

    JOptionPane

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,18 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean isInputValid = false; // A flag to control our loop
 
-        do {
-            try {
-                System.out.print("Please enter an integer numerator: ");
-                int numerator = scanner.nextInt();
+        String name = JOptionPane.showInputDialog("What is your name?");
 
-                System.out.print("Please enter an integer denominator: ");
-                int denominator = scanner.nextInt();
+        String message = String.format("Welcome, %s, to Java Programming!", name);
 
-                int result = numerator / denominator;
-                System.out.printf("\nResult: %d / %d = %d\n", numerator, denominator, result);
-
-                isInputValid = true; // If we get here, the input was good, and we can exit the loop
-            }
-            catch (InputMismatchException e) {
-                System.err.println("\nError: You must enter integers. Please try again.\n");
-                scanner.nextLine(); // Important: clear the bad input from the scanner
-            }
-            catch (ArithmeticException e) {
-                System.err.println("\nError: Denominator cannot be zero. Please try again.\n");
-            }
-        } while (!isInputValid);
+        JOptionPane.showMessageDialog(null, message);
     }
 }
