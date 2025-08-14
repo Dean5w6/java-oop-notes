1ac8cd6c211b487d01bc82ccfd1cf857ebc61d3c
Dean5w6 <deanjoefrey@gmail.com>
2025-08-14 21:56:13 +0800

    Interactive GUI click counter

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,19 @@ import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Interactive GUI");
-        JLabel label = new JLabel("Press the button.");
-        JButton button = new JButton("Click Me");
+        JFrame frame = new JFrame("Click counter");
+        JLabel label = new JLabel("Click the button!");
+        JButton button = new JButton("Click me");
 
         ActionListener listener = new ActionListener() {
+            int counter = 0;
             @Override
+
             public void actionPerformed(ActionEvent e) {
-                label.setText("Button was clicked!");
+                counter++;
+                label.setText("Button was clicked! Clicks: " + counter);
             }
         };
-
         button.addActionListener(listener);
 
         frame.setLayout(new FlowLayout());
