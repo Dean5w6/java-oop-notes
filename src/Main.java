4b5176f11bbeb3e80c4270f7cf70282d79b4d206
Dean5w6 <deanjoefrey@gmail.com>
2025-08-14 22:06:52 +0800

    ArrayList

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,22 @@ import javax.swing.*;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Click counter");
-        JLabel label = new JLabel("Click the button!");
-        JButton button = new JButton("Click me");
+        ArrayList<String> names = new ArrayList<>();
 
-        ActionListener listener = new ActionListener() {
-            int counter = 0;
-            @Override
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Charlie");
 
-            public void actionPerformed(ActionEvent e) {
-                counter++;
-                label.setText("Button was clicked! Clicks: " + counter);
-            }
-        };
-        button.addActionListener(listener);
+        System.out.println("The first name is: " + names.get(0));
 
-        frame.setLayout(new FlowLayout());
-        frame.add(label);
-        frame.add(button);
+        System.out.println("The list has: " + names.size() + " names.");
 
-        frame.setSize(300, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
