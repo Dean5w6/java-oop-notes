c600052637d52ca968261bf24a9592d8ea1913b6
Dean5w6 <deanjoefrey@gmail.com>
2025-08-15 18:42:01 +0800

    Simple To-Do List Program

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,40 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> tasks = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        String action = "";
 
-        names.add("Alice");
-        names.add("Bob");
-        names.add("Charlie");
+        do {
+            System.out.println("\n[Simple To-Do List Program]");
+            System.out.println("Usage: add [task], list, quit");
+            System.out.printf("> ");
+            action = scanner.nextLine();
 
-        System.out.println("The first name is: " + names.get(0));
+            if (action.startsWith("add ")) {
+                String newTask = action.substring(4);
 
-        System.out.println("The list has: " + names.size() + " names.");
+                if (newTask.isBlank()) {
+                    System.out.println("Task can't be blank!");
+                } else {
+                    tasks.add(newTask);
+                    System.out.println("Task " + newTask + " was added successfully.");
+                }
+            } else if (action.equals("list")) {
+                if (tasks.isEmpty()) {
+                    System.out.println("No tasks yet.");
+                } else {
+                    System.out.println("\n--- Your Tasks ---");
 
-        for (String name : names) {
-            System.out.println(name);
-        }
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println("\t" + (i+1) + ". " + tasks.get(i));
+                    }
+                }
+            } else if (action.equals("quit")) {
+                System.out.println("Quitting the program...");
+            } else {
+                System.out.println("Not a valid action.");
+            }
+        } while(!action.equals("quit"));
     }
 }
