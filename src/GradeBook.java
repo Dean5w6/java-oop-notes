d4acb723bda23a6e6b342ad40ebbbe4c18907fc4
Dean5w6 <deanjoefrey@gmail.com>
2025-08-16 01:11:26 +0800

    Exercise: Using Methods with Parameters

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -1,5 +1,5 @@
 public class GradeBook {
-    public void displayMessage() {
-        System.out.println("Welcome to the Grade Book!");
+    public void displayMessage(String courseName) {
+        System.out.println("Welcome to the grade book for " + courseName + "!");
     }
 }
--- src/GradeBookTest.java
+++ src/GradeBookTest.java
@@ -1,6 +1,6 @@
 public class GradeBookTest {
     public static void main(String[] args) {
         GradeBook gradeBook = new GradeBook();
-        gradeBook.displayMessage();
+        gradeBook.displayMessage("CS101 Introduction to Java Programming");
     }
 }
