13e10d3003219d4e7774f13f63e64c630358b282
Dean5w6 <deanjoefrey@gmail.com>
2025-08-16 01:46:15 +0800

    Setter and Getter + Scanner

--- src/GradeBookTest.java
+++ src/GradeBookTest.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class GradeBookTest {
     public static void main(String[] args) {
         GradeBook gradeBook = new GradeBook();
+        Scanner scanner = new Scanner(System.in);
 
-        gradeBook.setCourseName("Objected Oriented Programming");
+        System.out.println("Initial course name is: " + gradeBook.getCourseName());
+        // would return "null"
+
+        System.out.printf("Enter the course name: ");
+        String newCourseName = scanner.nextLine();
+
+        gradeBook.setCourseName(newCourseName);
         gradeBook.displayMessage();
     }
 }
