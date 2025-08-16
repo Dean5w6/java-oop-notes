17b4c9441dca72f35ba01e9217b364b4b9117e54
Dean5w6 <deanjoefreyii@gmail.com>
2025-08-16 14:59:12 +0800

    GPA calculator exercise

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GradeBook {
     private String courseName;
 
@@ -13,6 +15,39 @@ public class GradeBook {
 
     public String getCourseName() {
         return courseName;
+    }
+
+    public void determineClassAverage() {
+        Scanner scanner = new Scanner(System.in);
+        double total = 0, grade = 0.0;
+        int units = 0, totalUnits = 0;
+
+        while (true) {
+            System.out.printf("Enter a grade (Type '0' to end): ");
+            grade = scanner.nextDouble();
+            if (grade < 1.0) {
+                System.out.println("Invalid grade.");
+                grade = 0.0;
+            }
+
+            System.out.printf("Enter the number of units for that grade (Type '0' to end): ");
+            units = scanner.nextInt();
+            if (units < 1) {
+                System.out.println("Invalid units.");
+                units = 0;
+            }
+
+            if (grade >= 1.0 && units >= 1) {
+                totalUnits += units;
+                total += (grade * units);
+            } else if (grade < 1.0 || units < 1) {
+                break;
+            }
+        }
+
+        double GPA = (total / totalUnits);
+        System.out.println("\nTotal grades: " + total + "\nTotal units: " + totalUnits);
+        System.out.println("Your GPA is: " + GPA);
     }
 
     public void displayMessage() {
--- src/GradeBookTest.java
+++ src/GradeBookTest.java
@@ -3,15 +3,6 @@ import java.util.Scanner;
 public class GradeBookTest {
     public static void main(String[] args) {
         GradeBook gradeBook = new GradeBook("Object-oriented Programming");
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Initial course name is: " + gradeBook.getCourseName());
-        // would return "null" without the constructor
-
-        System.out.printf("Enter the course name: ");
-        String newCourseName = scanner.nextLine();
-
-        gradeBook.setCourseName(newCourseName);
-        gradeBook.displayMessage();
+        gradeBook.determineClassAverage();
     }
 }
